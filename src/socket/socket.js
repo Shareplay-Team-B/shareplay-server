27de4059e2377d03921e85b26911514b1394848b
Varun Vudathu <varun.vudathu@gmail.com>
2022-10-28 18:10:03 -0400

    syncing

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -12,23 +12,20 @@ const createServer = (httpServer) => {
     },
   });
 
-  io.on('connection', (client) => {
-    // console.log('client connected:', client.id);
+  io.on('connect', (client) => {
+    console.log('client connected:', client.id);
 
     client.on('disconnect', () => {
-      // console.log('client disconnected:', client.id);
+      console.log('client disconnected:', client.id);
     });
 
     client.on('test-channel', (data) => {
-      // console.log('received socket data from client:', JSON.stringify(data));
-      JSON.stringify(data);
+      console.log('received socket data from client:', JSON.stringify(data));
     });
 
     client.on('video-update', (data) => {
-      // console.log('received socket data from client:', JSON.stringify(data));
-      JSON.stringify(data);
       console.log('sending data: ', data);
-      client.broadcast.emit(data);
+      client.broadcast.emit('video-update-client', data);
     });
   });
 };
