45c85423c047c92d8acbc7d65d4a05f140d71183
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-11-27 18:36:46 -0500

    Fixed port listening

--- src/server.js
+++ src/server.js
@@ -28,6 +28,6 @@ admin.initializeApp({
 // routes
 app.use(AuthController);
 // create web socket server
-const server = app.listen(process.env.port || 8080);
+const server = app.listen(process.env.PORT || 8080);
 socket.createServer(server);
 console.log(`App listening on port ${server.address().port}`);
